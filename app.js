311fd8ef558f96620cc1401704a594671716cebd
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-02-13 22:35:48 +0300

    Array Types

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string,
+//     age: number
+// } = {
 var person = {
     name: 'Ismail',
-    age: 31
+    age: 31,
+    hobbies: ['Painting', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -4,7 +4,16 @@
 // } = {
 const person = {
     name: 'Ismail',
-    age: 31
+    age: 31,
+    hobbies: ['Painting', 'Cooking']
 }
 
+let favoriteActivities: string[]
+favoriteActivities = ['Sports']
+
 console.log(person.name)
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+    // console.log(hobby.map()) => ERROR : map is only applicable with arrays
+}
