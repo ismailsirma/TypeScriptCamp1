4e36bcf5ea8115b045c60733bab8dde611f144bd
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-02-13 22:45:43 +0300

    Tuple example

--- app.ts
+++ app.ts
@@ -2,11 +2,21 @@
 //     name: string,
 //     age: number
 // } = {
-const person = {
+const person: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string] // Tuple Type
+} = {
     name: 'Ismail',
     age: 31,
-    hobbies: ['Painting', 'Cooking']
+    hobbies: ['Painting', 'Cooking'],
+    role: [2, 'author']
 }
+
+//person.role.push('admin')
+//person.role[1] = 10
+person.role = [1, 'admin']
 
 let favoriteActivities: string[]
 favoriteActivities = ['Sports']
