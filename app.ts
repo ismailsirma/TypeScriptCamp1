6dfce3149496ac36aa834eed0633d6591d769b7d
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-02-14 16:26:05 +0300

    Literal Types

--- app.ts
+++ app.ts
@@ -1,15 +1,20 @@
-function combine(input1: number | string, input2: number | string){
+function combine(
+    input1: number | string, 
+    input2: number | string,
+    // allow only two possible value strings:
+    resultConversion: 'as-number' | 'as-text'
+    ){
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2
     } else {
         result = input1.toString() + input2.toString()
     }
     return result
 }
 
-const combinedAges = combine(31, 26)
+const combinedAges = combine(31, 26, 'as-number')
 console.log(combinedAges)
 
-const combinedNames = combine('Ismail', 'Anna')
+const combinedNames = combine('Ismail', 'Anna', 'as-text')
 console.log(combinedNames)
