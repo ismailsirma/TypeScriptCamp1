e21a3ab2b052c926fd80fe37c165c1063477cf9f
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-07-17 16:15:29 +0300

    constructor functions & this keyword

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,8 @@ class Department {
 const tech = new Department('Tech');
 tech.describe()
 
-// this keyword not works with passing a method pointer into an object not works
+// this keyword not works with passing a method pointer into an object not works 
+// without proving info about this keyword
+// should add definition of this as a parameter
 const techCopy = {name: 'COPY',describe: tech.describe}
 techCopy.describe()
