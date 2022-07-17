e386a15d27ccca845992eb25721570a2168cc269
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-07-17 17:22:05 +0300

    private access modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,36 @@
 class Department {
     name: string
+    private employees: string[] = []
 
     constructor(n: string) {
         this.name = n
     }
 
     describe(this: Department) {
+        // validation here
         console.log('Department: ' + this.name)
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length)
+        console.log(this.employees)
     }
 }
 
 const tech = new Department('Tech');
+tech.addEmployee('Ismail')
+tech.addEmployee('Erdem')
+
 tech.describe()
+tech.printEmployeeInformation()
 
 // this keyword not works with passing a method pointer into an object not works 
 // without proving info about this keyword
 // should add definition of this as a parameter
-const techCopy = {name: 'COPY',describe: tech.describe}
-techCopy.describe()
+//  const techCopy = {name: 'COPY',describe: tech.describe}
+//  techCopy.describe()
+
