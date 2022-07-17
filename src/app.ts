04341e779006c6dbeee49405f1bc994183f6fcec
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-07-17 22:35:18 +0300

    using interfaces with classes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
-interface Person {
+interface Greetable {
     name: string
-    age: number
 
     greet(phrase: string): void
 }
 
-let user1: Person
-user1 = {
-    name: 'Ismail',
-    age: 30,
+class Person implements Greetable {
+    name: string
+    age = 30
+
+    constructor(n: string) {
+        this.name = n
+    }
+
     greet(phrase: string) {
         console.log(phrase + ' ' + this.name)
     }
 }
 
+let user1: Greetable
+user1 = new Person('Ismail')
+
 user1.greet('Hi There - My name is')
+console.log(user1)
