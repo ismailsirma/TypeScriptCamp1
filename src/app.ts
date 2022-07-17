c23a1f83785697457ecf323ed10f17e911ad2697
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-07-17 22:57:21 +0300

    interfaces as function types

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+//type AddFn = (a: number, b: number) => number
+interface AddFn {
+    (a: number, b: number): number
+}
+
+let add1: AddFn
+
+add1 = (n1: number, n2: number) => {
+    return n1 + n2
+}
+
 interface Named {
     readonly name: string
 }
