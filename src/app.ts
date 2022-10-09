44bb5c41e04f33b4686d9e0c5687b2eb848234ad
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-10-09 19:16:21 +0300

    generic functions

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // Generics Type
 
 //const names = ['Ismail', 'Erdem']
-const names: Array<string> = []
+const names: Array<string> = ['']
 names[0].split(' ')
 
 // promise object is created
@@ -18,3 +18,15 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then(data => {
     data.split(' ')
 })
+
+// GENERIC FUNCTIONS
+
+// we dont care about exact object type of parameters
+//function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T, U>(objA: T, objB: U) {
+
+    return Object.assign(objA, objB)
+}
+
+const mergedObj = merge<{name:string}, {age: number}>({name: 'Erdem'}, {age: 30})
+console.log(mergedObj)
