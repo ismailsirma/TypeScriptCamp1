e2c8307f2a64388f2ebfe4b51f81982872b9bfa3
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-10-10 15:17:16 +0300

    decorator factories

--- src/app.ts
+++ src/app.ts
@@ -15,3 +15,23 @@ class Human {
 
 const pers = new Human()
 console.log(pers) 
+
+// Decorator Factory
+function Loggerv2(loggingString: string) {
+    return function(constructor: Function) {
+        console.log(loggingString)
+        console.log(constructor)
+    }
+}
+
+ @Loggerv2('Logging - HUMAN')
+class Humanv2 {
+    name = 'Ismail'
+
+    constructor() {
+        console.log('Creating a human object...')
+    }
+}
+
+const hum = new Humanv2()
+console.log(hum) 
