1f739cd95d93da779424e8861411c69dcea57e92
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-10-10 20:47:44 +0300

    property decorators

--- src/app.ts
+++ src/app.ts
@@ -59,3 +59,36 @@ class Test {
         console.log('creating a test obj...')
     }
 }
+
+
+// Property Decorators
+
+// target gets the prototype of the class, its method definitions
+// decorator gets executed when class definition is registered in app context
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!')
+    console.log(target, propertyName)
+}
+
+class Product {
+    @Log
+    title: string
+    private _price: number
+
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val
+        } else {
+            throw new Error('Invalid price - should be positive!')
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t
+        this._price = p
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax)
+    }
+}
